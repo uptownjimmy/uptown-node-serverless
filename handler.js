e7a65c59bbabfa16ffd5139e6550703c1faf1b64
James McRay Johnson III <james@uptownjimmy.com>
2020-05-03 08:58:28 -0400

    Initial commit

---
+++ handler.js
@@ -0,0 +1,111 @@
+const db = require('./db_connect');
+
+'use strict';
+
+// module.exports.hello = async event => {
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify(
+//       {
+//         message: 'Go Serverless v1.0! Your function executed successfully!',
+//         input: event,
+//       },
+//       null,
+//       2
+//     ),
+//   };
+// };
+
+module.exports.getAllItems = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  db.getAll('items')
+    .then((res) => {
+      callback(null, {
+        statusCode: 200,
+        headers: {},
+        body: JSON.stringify(res),
+      });
+    })
+    .catch((e) => {
+      console.log(e);
+      callback(null, {
+        statusCode: e.statusCode || 500,
+        body: 'Error: Could not find Items: ' + e,
+      });
+    });
+};
+
+module.exports.getItem = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  db.getById('items', event.pathParameters.id)
+    .then((res) => {
+      callback(null, {
+        statusCode: 200,
+        headers: {},
+        body: JSON.stringify(res),
+      });
+    })
+    .catch((e) => {
+      console.log(e);
+      callback(null, {
+        statusCode: e.statusCode || 500,
+        body: 'Could not find Item: ' + e,
+      });
+    });
+};
+
+module.exports.createItem = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  const data = JSON.parse(event.body);
+  db.insert('items', data)
+    .then((res) => {
+      callback(null, {
+        statusCode: 200,
+        headers: {},
+        body: 'Item Created: ' + res,
+      });
+    })
+    .catch((e) => {
+      callback(null, {
+        statusCode: e.statusCode || 500,
+        body: 'Could not create Item ' + e,
+      });
+    });
+};
+
+module.exports.updateItem = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  const data = JSON.parse(event.body);
+  db.updateById('items', event.pathParameters.id, data)
+    .then((res) => {
+      callback(null, {
+        statusCode: 200,
+        headers: {},
+        body: 'Item Updated: ' + res,
+      });
+    })
+    .catch((e) => {
+      callback(null, {
+        statusCode: e.statusCode || 500,
+        body: 'Could not update Item' + e,
+      });
+    });
+};
+
+module.exports.deleteItem = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  db.deleteById('items', event.pathParameters.id)
+    .then((res) => {
+      callback(null, {
+        statusCode: 200,
+        headers: {},
+        body: 'Item Deleted: ',
+      });
+    })
+    .catch((e) => {
+      callback(null, {
+        statusCode: e.statusCode || 500,
+        body: 'Could not delete Item ' + e,
+      });
+    });
+};
